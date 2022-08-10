fb5ee3ede3407cd92786d6a088be157597d6c76d
3011 <choybb@qq.com>
2022-08-10 16:13:48 +0800

    更新接口

--- app/models.py
+++ app/models.py
@@ -4,19 +4,52 @@ from django.db import models
 # Create your models here.
 
 
-class Error(models.Model):
-    title = models.CharField(max_length=100)
-    url = models.CharField(max_length=1000)
-    timestamp = models.DecimalField(max_digits=15, decimal_places=0)
-    full_ua = models.CharField(max_length=100)
-    browser_name = models.CharField(max_length=100)
-    browse_version = models.CharField(max_length=100)
-    os = models.CharField(max_length=100)
+class ErrorPublic(models.Model):
+    # 公共字段
+    domain = models.TextField()
+    title = models.TextField()
+    url = models.TextField()
+    timestamp = models.BigIntegerField()
+    full_ua = models.TextField()
+    browser_name = models.TextField()
+    browse_version = models.TextField()
+    os = models.TextField()
+    # type = models.TextField()
+    # kind = models.TextField()
+    # error_type = models.TextField()
+    message = models.TextField()
 
-    # type = models.CharField(max_length=100)
-    error_type = models.CharField(max_length=100)
-    kind = models.CharField(max_length=100)
-    message = models.CharField(max_length=1000)
-    position = models.CharField(max_length=100)  # 3,4 行与列
-    stack = models.CharField(max_length=1000)
-    selector = models.CharField(max_length=1000)
+    class Meta:
+        abstract = True
+
+
+class JSError(ErrorPublic):
+    position = models.TextField()  # 3:4 行与列
+    stack = models.TextField()
+    selector = models.TextField()
+
+
+class PromiseError(ErrorPublic):
+    stack = models.TextField()
+    selector = models.TextField()
+
+
+class ResourceError(ErrorPublic):
+    filename = models.TextField()
+    tag_name = models.TextField()
+    position = models.TextField()
+    selector = models.TextField()
+
+
+class XhrError(ErrorPublic):
+    status = models.TextField()
+    duration = models.IntegerField()
+    response = models.TextField()
+    params = models.TextField()
+
+
+class WhiteScreenError(ErrorPublic):
+    empty_points = models.TextField()
+    screen = models.TextField()
+    view_point = models.TextField()
+    selector = models.TextField()
--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 # from django.shortcuts import render
+from email.policy import default
 from django.http import HttpResponse
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
-from app.models import Error
+import app.models as my_models
 import json
+import time
 
 
 def response_success(msg):
@@ -18,6 +20,10 @@ def response_success_with_data(msg, data):
     return HttpResponse(json.dumps({"ok": 1, "err_type": "", "msg": msg, "data": data}, ensure_ascii=False))
 
 
+def response_fail_with_data(err_type, msg):
+    return HttpResponse(json.dumps({"ok": 0, "err_type": err_type, "msg": msg, "data": []}, ensure_ascii=False))
+
+
 @csrf_exempt
 def post_err(request):
     if request.method != 'POST':
@@ -25,30 +31,79 @@ def post_err(request):
 
     try:
         json_data = json.loads(request.body)
-        print(json_data)
     except:
         return response_fail("JSONError", "JSON格式有误")
 
     #  TODO: JSON数据校验
     try:
-        newErr = Error(title=json_data["title"],
-                       url=json_data["url"],
-                       timestamp=json_data["timestamp"],
-                       full_ua=json_data["userAgent"]["full"],
-                       browser_name=json_data["userAgent"]["name"],
-                       browse_version=json_data["userAgent"]["version"],
-                       os=json_data["userAgent"]["os"],
 
-                       error_type=json_data["errorType"],
-                       kind=json_data["kind"],
-                       message=json_data["message"],
-                       position=json_data["position"],
-                       stack=json_data["stack"],
-                       selector=json_data["selector"],
-                       )
-        newErr.save()  # 保存至数据库
+        pub_data = {
+            "title": json_data["title"],
+            "url": json_data["url"],
+            "timestamp": json_data["timestamp"],
+            "full_ua": json_data["userAgent"]["full"],
+            "browser_name": json_data["userAgent"]["name"],
+            "browse_version": json_data["userAgent"]["version"],
+            "os": json_data["userAgent"]["os"],
+            "message": json_data["message"],
+            # "error_type": json_data["type"],
+            # "kind": json_data["kind"],
+        }
+
+        if json_data["errorType"] == "jsError":
+            new_Error = my_models.JSError(
+                position=json_data["position"],
+                stack=json_data["stack"],
+                selector=json_data["selector"],
+                **pub_data,
+            )
+            new_Error.save()  # 保存至数据库
+
+        elif json_data["errorType"] == "promiseError":
+            new_Error = my_models.PromiseError(
+                stack=json_data["stack"],
+                selector=json_data["selector"],
+                **pub_data,
+            )
+            new_Error.save()  # 保存至数据库
+
+        elif json_data["errorType"] == "resourceError":
+            new_Error = my_models.ResourceError(
+                filename=json_data["filename"],
+                tag_name=json_data["tagName"],
+                position=json_data["position"],
+                selector=json_data["selector"],
+                **pub_data,
+            )
+            new_Error.save()  # 保存至数据库
+
+        elif json_data["errorType"] == "xhrError":
+            new_Error = my_models.XhrError(
+                status=json_data["status"],
+                duration=json_data["duration"],
+                response=json_data["response"],
+                params=json_data["params"],
+                **pub_data,
+            )
+            new_Error.save()  # 保存至数据库
+
+        elif json_data["errorType"] == "whiteScreenError":
+            new_Error = my_models.WhiteScreenError(
+                empty_points=json_data["emptyPoints"],
+                screen=json_data["screen"],
+                view_point=json_data["viewPoint"],
+                selector=json_data["params"],
+                **pub_data,
+            )
+            new_Error.save()  # 保存至数据库
+
+        else:
+            return response_fail("TypeError", "未知类型")
+
         return response_success("成功")
+
     except Exception as err:
+        print(err)
         return response_fail(type(err).__name__, repr(err))
 
 
@@ -56,6 +111,30 @@ def get_all_err(request):
     if request.method != 'GET':
         return response_fail("MethodError", "不是GET请求")
 
-    data = serializers.serialize("json", Error.objects.all())
+    error_type = request.GET.get("errortype", default="")
+
+    if error_type == "jsError":
+        data = serializers.serialize("json", my_models.JSError.objects.all())
+
+    elif error_type == "promiseError":
+        data = serializers.serialize(
+            "json", my_models.PromiseError.objects.all())
+
+    elif error_type == "resourceError":
+        data = serializers.serialize(
+            "json", my_models.ResourceError.objects.all())
+
+    elif error_type == "xhrError":
+        data = serializers.serialize("json", my_models.XhrError.objects.all())
+
+    elif error_type == "whiteScreenError":
+        data = serializers.serialize(
+            "json", my_models.WhiteScreenError.objects.all())
+
+    else:
+        return response_fail_with_data("TypeError", "未知类型")
+
     data = json.loads(data)
     return response_success_with_data("成功（测试接口）", data)
+
+# def get_err_by_browser_name
